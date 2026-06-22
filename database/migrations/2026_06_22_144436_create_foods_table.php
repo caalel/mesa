a936d4aa1ffaf47720578255005379bf5f30ca4e
Calel Marques <calelmarques@gmail.com>
2026-06-22 11:53:52 -0300

    feat: create foods table migration

---
+++ database/migrations/2026_06_22_144436_create_foods_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('calories_per_100g', 8, 2);
+            $table->decimal('protein_per_100g', 8, 2);
+            $table->decimal('carbs_per_100g', 8, 2);
+            $table->decimal('fat_per_100g', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('foods');
+    }
+};
